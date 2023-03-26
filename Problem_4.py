f6f32a3cb7f2df6bf1fd8a2bf80a93396e48aa73
praveen N <95226524+praveendecode@users.noreply.github.com>
2023-03-26 16:49:16 +0530

    Create Problem_4.py

---
+++ Problem_4.py
@@ -0,0 +1,9 @@
+# 4.write a program for natural number based on user input
+
+number = int(input("Enter the value :"))
+
+
+# Ternary operator
+
+print((f"The given number {number} is a natural number")) if number>-1 else print((f"The given number {number} is not  a natural number"))
+
