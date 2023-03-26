2af261ba393a0096c04d401955f28ec78361c57a
praveen N <95226524+praveendecode@users.noreply.github.com>
2023-03-26 16:46:45 +0530

    Create Problem_3.py

---
+++ Problem_3.py
@@ -0,0 +1,5 @@
+# 3.urlist = ['wood','knife','axe'] , mylist = ['tree', 'apple', 'mango', 'melon'] – combine two lists
+
+urlist = ['wood','knife','axe']
+mylist = ['tree', 'apple', 'mango', 'melon']
+print(urlist+mylist)
