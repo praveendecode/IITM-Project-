b175d1f4d10b33e6acd462533ffcec8696fd4447
praveen N <95226524+praveendecode@users.noreply.github.com>
2023-03-26 16:41:13 +0530

    Create Problem_1.py

---
+++ Problem_1.py
@@ -0,0 +1,19 @@
+# Prime Number for a given range
+Start = int(input("Enter the start range:"))
+End = int(input("Enter the start range:"))
+print("The Prime numbers of given range is:")
+
+count = 0
+for i in range(Start, End):
+    prime = 0
+    for j in range(2,i):
+        if i % j == 0:
+            prime = 1
+            break
+    if prime == 0 :
+        count +=1
+        if i == 1:
+           continue
+        print(i, end=" ")
+print()
+print(f"The count of Total Prime Numbers from range {Start} to {End} is {count}")
